868c7fd90516332e2a78ef5bcd4bfe22117a893d
Timofey <buzmakov1987@gmail.com>
2021-03-31 21:50:06 -0700

    Created reacttypescript app

---
+++ social-network-typescript/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+    <div>
+    
+    <h1>Social Network!</h1>
+    <p>Lets get started!</p>
+
+    </div>
+    
+    );
+}
+
+export default App;
